b1e0813c0bb009b65f2f8943f11d67025655df42
mbuckles <bucklesm21@gmail.com>
2017-03-30 16:17:03 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var useragent = require('express-useragent')
+//create express app, instantiate cors & body-parser.
+var app = module.exports = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use(useragent.express());
+
+var api = '/api/whoami';
+
+app.get(api, function(req, res, next){
+
+var language = req.acceptsLanguages();
+var software = "Os: " + req.useragent.os;
+//req.header['user-agent']
+var ipaddress = rep.ip;
+
+res.json({'ipaddress': ipaddress, 'language': language[0], 'software': software});
+});
+
+app.listen(3000);
