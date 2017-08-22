3038784a6cbfc87cfbf1244d9f7fa6a2d0427965
mbuckles <mbuckles@users.noreply.github.com>
2017-08-22 10:42:42 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
   res.render('index', { title: 'Welcome to my Header-Parser API' });
-=======
-  res.render('index', { title: 'Express' });
->>>>>>> f7a4ab0572720d909352a27c790bdfd5c8108b84
 });
 
 module.exports = router;
